15cd0b2a2805dd8ba82f19ca6589d711a4c8263a
Ali Khaladj <ali@khaladj.ca>
2021-03-08 10:21:52 -0500

    merged

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
-<?
+<?php
 
-class Github{
+class Github
+{
+    private $array;
 
-	private $array;
+    public function __construct($array)
+    {
+        $this->setArray($array);
+    }
+    
+    
+    public function setArray($array)
+    {
+        $this->array = $array;
+    }
 
-	function __construct($array){
-			$this->setArray($array);
-	}
-	
-	
-	function setArray($array){
-		$this->array = $array; 
-	}
-
-	
-
-	function print_array(){
-		print_r($this->array);
-	}
-
+  
+    public function print_array()
+    {
+        print_r($this->array);
+    }
 }
